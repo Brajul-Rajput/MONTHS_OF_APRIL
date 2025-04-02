122752421dfb6b41e9fb0c7924cb4b04ddc8f08d
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-04-02 22:17:21 +0530

    Create Area.java

---
+++ ONE/Area.java
@@ -0,0 +1,13 @@
+                            /*     Area of the circle           */
+package ONE;
+
+public class Area {
+    public static void main(String[]args){
+        double radius=23;
+        float pi=3.14f;
+        double area=pi*radius*radius;
+        System.out.println("Area of the circle : "+area);
+
+
+    }
+}
