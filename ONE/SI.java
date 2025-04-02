a9bc79d5aaa8dec80b8b061bf2b3ba65ad1d79f4
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-04-02 22:17:03 +0530

    Create SI.java

---
+++ ONE/SI.java
@@ -0,0 +1,10 @@
+package ONE;
+class SI{
+    public static void main(String[]args){
+        int p=1000;
+        int r=10;
+        int t=3;
+        double si=p*r*t/100;
+        System.out.println("simple interest is "+si);
+    }
+}
